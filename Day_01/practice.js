a3cf15196fe062e491f0b200ef053fc70723e317
sufiyan10-1 <134135723+sufiyan10-1@users.noreply.github.com>
2024-07-14 10:51:55 +0530

    day 01 is added

---
+++ Day_01/practice.js
@@ -0,0 +1,36 @@
+//task 1
+var mynumber = 30;
+console.log(myNumber);
+
+//task 2
+let myString = "chaiCode";
+console.log(myString);
+
+//task 3
+const myBooleanValue = true;
+console.log(myBooleanValue);
+
+//task 4
+//typeOf opertor: it is used to determine the type of a variable or an expression
+const testNumber= 10;
+const testString = "chaiCode"
+const testBoolean = false;
+const testObject = {"fist": 1,  "second" : 2};
+const testArray =  ["first", 1, "second", 2];
+console.log(typeof testNumber); //number
+console.log(typeof testString); //string
+console.log(typeof testBoolean); // booean
+console.log(typeof testObject); // object
+console.log(typeof testArray);  // object :  because In JavaScript, arrays are a specialized kind of object
+
+//task 5
+let letValue = "chai"
+console.log(letValue); //code
+letValue = "our code"
+console.log(letValue); //with chai
+
+//task 5
+const  constValue = "channel"
+console.log(constValue); //code
+constValue = "subscriber"                               //can not reasign to constant varible 
+console.log(constValue); //error: Assignment to constant variable :beacuse of const keyword verible become constant hence we
